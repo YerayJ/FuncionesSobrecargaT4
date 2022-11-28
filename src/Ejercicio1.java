421f420713897f03e6aebaf2e082fd64e1257010
YerayJ <yerayjimeenez07@gmail.com>
2022-11-28 10:45:37 +0100

    first commit

---
+++ src/Ejercicio1.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    // Creamos función de tipo int le pedimos dos valores de tipo int y devolvemos
+    // la suma de ellos
+    static int suma(int num1, int num2) {
+        return num1 + num2;
+    }
+    // Creamos función de tipo double le pedimos dos valores de tipo double y devolvemos
+    // la suma de ellos
+    static double suma(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public static void main(String[] args) {
+        // Creamos variables de tipo int
+        int num1int, num2int;
+        // Creamos variables de tipo double
+        double num1double, num2double;
+
+        // Creamos escaner
+        Scanner key = new Scanner(System.in);
+
+        // Pedimos variables al usuario de tipo int y double
+        System.out.println("Deme dos variables de tipo int: ");
+        num1int = key.nextInt();
+        num2int = key.nextInt();
+        System.out.println("Deme dos variables de tipo double: ");
+        num1double = key.nextDouble();
+        num2double = key.nextDouble();
+
+        System.out.println("El resultado de tipo integer es: " + suma(num1int, num2int));
+        System.out.println("El resultado de tipo integer es: " + suma(num1double, num2double));
+    }
+
+}
